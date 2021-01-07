b9a0a578638878ff41a115f5ff02cf8f2dd93344
Ahmad Al-Sharif <seaskyways@gmail.com>
2021-01-07 21:10:22 +0200

    added github link

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <v-app>
     <v-app-bar app color="primary" dense>
       <v-app-bar-title>Ahmad Al-Sharif's Profile</v-app-bar-title>
+      <v-spacer />
+      <v-btn
+        href="https://github.com/seaskyways/profile"
+        target="_blank"
+        depressed
+      >
+        <v-icon class="me-1">mdi-github</v-icon> Github
+      </v-btn>
     </v-app-bar>
     <v-main class="teal lighten-2">
       <router-view />
